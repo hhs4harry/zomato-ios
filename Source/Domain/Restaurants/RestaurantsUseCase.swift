78fc33bcd919a96582d55364d734d51aa446b52d
Harry <hhs4harry@gmail.com>
2018-09-24 05:15:56 +1000

    Add JSON cache

--- Source/Domain/Restaurants/RestaurantsUseCase.swift
+++ Source/Domain/Restaurants/RestaurantsUseCase.swift
@@ -17,11 +17,13 @@ final class RestaurantsUseCaseImplementation: RestaurantsUseCase {
     // MARK: Private
 
     private let repository: RestaurantsRepository
+    private let cacheClient: JSONCache
 
     // MARK: Initialise
 
-    init(repository: RestaurantsRepository) {
+    init(repository: RestaurantsRepository, cacheClient: JSONCache) {
         self.repository = repository
+        self.cacheClient = cacheClient
     }
 
     // MARK: - Conformance
@@ -29,8 +31,30 @@ final class RestaurantsUseCaseImplementation: RestaurantsUseCase {
     // MARK: RestaurantsUseCase
 
     func bestRestaurants(forCity city: City) -> SignalProducer<[Restaurant], RepositoryError> {
-        return repository.bestRestaurants(forCity: city)
-            .map { $0.sorted(by: { Double($0.rating.aggregate) ?? 0.0 > Double($1.rating.aggregate) ?? 0.0 }) }
+        return SignalProducer<[Restaurant], RepositoryError> { [unowned self] observer, lifetime in
+            let cacheKey: String = "\(city.name)_\(String(city.id))"
+            if let cache: [Restaurant] = self.cacheClient.retrive(itemNamed: cacheKey) {
+                observer.send(value: cache)
+            }
+
+            let disposable: Disposable = self.repository.bestRestaurants(forCity: city)
+                .on(
+                    failed: { _ in
+                        observer.sendCompleted()
+                    },
+                    value: { result in
+                        self.cacheClient.store(result, named: cacheKey)
+                        observer.send(value: result)
+                        observer.sendCompleted()
+                    }
+                )
+                .start()
+
+            lifetime.observeEnded {
+                disposable.dispose()
+            }
+        }
+        .map { $0.sorted(by: { Double($0.rating.aggregate) ?? 0.0 > Double($1.rating.aggregate) ?? 0.0 }) }
     }
 }
 
@@ -39,7 +63,10 @@ final class RestaurantsUseCaseImplementation: RestaurantsUseCase {
 extension RestaurantsUseCaseImplementation: DependencyInjectionAware {
     static func register(in container: Container) {
         container.register(RestaurantsUseCase.self) { resolver in
-            RestaurantsUseCaseImplementation(repository: resolver.resolve(RestaurantsRepository.self)!)
+            RestaurantsUseCaseImplementation(
+                repository: resolver.resolve(RestaurantsRepository.self)!,
+                cacheClient: resolver.resolve(JSONCache.self)!
+            )
         }.inObjectScope(.transient)
     }
 }
